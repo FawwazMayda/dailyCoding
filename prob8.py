7f0a5959a4edb7f61e64479e4f966cd5fd7b98b4
FawwazMayda <maydafawwaz@gmail.com>
2020-03-09 09:36:06 +0000

    Adding test and Bug Fixes

--- prob8.py
+++ prob8.py
@@ -1,38 +1,41 @@
 class Node:
   def __init__(self,d):
-    this.left = null
-    this.right = null
-    this.data = d
+    self.left = None
+    self.right = None
+    self.data = d
+  def __repr__(self):
+    return str(self.data)
 
 def countUnival(root):
-  if(root==null):
+  if(root==None):
     return 0
-  if (root.left==null and root.right==null):
+  if (root.left==None and root.right==None):
     return 1
-  if(root.left==null and root.data==root.right.data):
+  if(root.left==None and root.data==root.right.data):
     return countUnival(root.right) + 1
-  if(root.right==null and root.data==root.left.data):
+  if(root.right==None and root.data==root.left.data):
     return countUnival(root.left) + 1
   count = countUnival(root.left) + countUnival(root.right)
   if(root.left.data==root.data and root.right.data==root.data):
     count+=1
   return count
 
-  a = Node(0)
-  b = Node(1)
-  c = Node(0)
-  d = Node(1)
-  e = Node(0)
-  f = Node(1)
-  g = Node(1)
+a = Node(0)
+b = Node(1)
+c = Node(0)
+d = Node(1)
+e = Node(0)
+f = Node(1)
+g = Node(1)
 
-  d.left = f
-  d.right = g
+d.left = f
+d.right = g
 
-  c.left = d 
-  c.right = e 
+c.left = d 
+c.right = e 
 
-  a.left = b 
-  a.right = c 
+a.left = b 
+a.right = c 
 
-  print(countUnival(g))
+print(countUnival(g))
+print(countUnival(a))
