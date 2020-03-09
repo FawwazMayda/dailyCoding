81e87d44ac372e76bd03030542db8f1ac579751e
FawwazMayda <maydafawwaz@gmail.com>
2020-03-09 09:33:12 +0000

    adding

--- prob8.py
+++ prob8.py
@@ -11,9 +11,28 @@ def countUnival(root):
     return 1
   if(root.left==null and root.data==root.right.data):
     return countUnival(root.right) + 1
-  if(root.right==null and root.data=root.left.data):
+  if(root.right==null and root.data==root.left.data):
     return countUnival(root.left) + 1
   count = countUnival(root.left) + countUnival(root.right)
   if(root.left.data==root.data and root.right.data==root.data):
     count+=1
   return count
+
+  a = Node(0)
+  b = Node(1)
+  c = Node(0)
+  d = Node(1)
+  e = Node(0)
+  f = Node(1)
+  g = Node(1)
+
+  d.left = f
+  d.right = g
+
+  c.left = d 
+  c.right = e 
+
+  a.left = b 
+  a.right = c 
+
+  print(countUnival(g))
